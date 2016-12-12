3e94604f4e5d62fa6b3e84e32b54bab2bc8c70b3
louisGan <louisGan1988@gmail.com>
2016-12-12 10:26:59 +0800

    add cors package

--- server/config/express.js
+++ server/config/express.js
@@ -2,12 +2,14 @@
 
 import compression from 'compression'
 import express from 'express'
+import cors from 'cors'
 import path from 'path'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
 
 export default (app) => {
 
+    app.use(cors())
     app.use(compression())
     app.use(express.static(path.join(__dirname, '../../client')))
     app.use(morgan('dev'))
