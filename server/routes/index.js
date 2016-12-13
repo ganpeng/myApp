ba70ecd263f5eed140abac43464cf82dd01ba1db
louisGan <louisGan1988@gmail.com>
2016-12-13 16:45:53 +0800

    add pagination test api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,48 @@
 'use strict'
 
+
 import user from './user'
 import movie from './movie'
 import auth from './auth'
+
+import co from 'co'
+import db from '../models'
 
 export default (app) => {
 
     app.use('/api/movie', movie)
     app.use('/api/user', user)
     app.use('/api/auth', auth)
+
+    app.post('/api/test', (req, res) => {
+
+        co(function* () {
+
+            const { currentPage, perPage } = req.body
+
+            const movies = yield db.Movie.findAll({
+                limit : perPage,
+                offset : (currentPage - 1) * perPage
+            })
+            const count = yield db.Movie.count({})
+
+            if(movies) {
+                return res.json({
+                    success : true,
+                    totalPage : Math.ceil(count / perPage),
+                    currentPage : currentPage,
+                    movies
+                })
+            } else {
+                return res.json({
+                    success : false,
+                    error : '错误'
+                })
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    })
     
 }
